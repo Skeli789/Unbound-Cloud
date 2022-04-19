683251b804d9ab88aa9204e363d013273c207ffa
Skeli <17243618+Skeli789@users.noreply.github.com>
2022-04-18 22:24:07 -0400

    No Saving During Wonder Trade

--- src/MainPage.js
+++ src/MainPage.js
@@ -14,8 +14,8 @@ import {BoxList} from "./BoxList";
 import {BoxView, HIGHEST_HOME_BOX_NUM, MONS_PER_BOX, MONS_PER_COL, MONS_PER_ROW} from "./BoxView";
 import {/*ClearBrowserDB,*/ GetDBVal, SetDBVal} from "./BrowserDB";
 import {FriendTrade} from "./FriendTrade";
-import {DoesPokemonSpeciesExistInGame, GetIconSpeciesName, GetItem, GetSpecies, IsBlankMon,
-        IsHoldingBannedItem, PokemonAreDuplicates} from "./PokemonUtil";
+import {DoesPokemonSpeciesExistInGame, GetIconSpeciesName, GetIconSpeciesLink, GetItem, GetNickname,
+        GetSpecies, IsBlankMon, IsHoldingBannedItem, PokemonAreDuplicates} from "./PokemonUtil";
 import {BASE_GFX_LINK, CreateSingleBlankSelectedPos, GetBoxNumFromBoxOffset, GetBoxPosBoxColumn, GetBoxPosBoxRow,
         GetItemName, GetLocalBoxPosFromBoxOffset, GetOffsetFromBoxNumAndPos, GetSpeciesName} from "./Util";
 import SaveData from "./data/Test Output.json";
@@ -408,6 +408,17 @@ export default class MainPage extends Component
     }
 
     /**
+     * Gets a Pokemon in a specific box type and at a specific position in the entire list of boxes.
+     * @param {Number} boxType - Either BOX_HOME or BOX_SAVE.
+     * @param {Number} boxPos - A position from 0 until getBoxAmountByBoxSlot(boxSlot) - 1
+     * @returns {Pokemon} The Pokemon at the requested position.
+     */
+    getMonAtBoxPosByBoxType(boxType, boxPos)
+    {
+        return this.getBoxesByBoxType(boxType)[boxPos];
+    }
+
+    /**
      * Checks if a Pokemon has a duplicate in a specific set of boxes already.
      * @param {Pokemon} pokemon - The Pokemon to check.
      * @param {Array <Pokemon>} boxes - The boxes to check for the Pokemon.
@@ -478,6 +489,16 @@ export default class MainPage extends Component
             && this.state.wonderTradeData.boxType === boxType
             && this.state.wonderTradeData.boxNum === boxNum
             && this.state.wonderTradeData.boxPos === boxPos;
+    }
+
+    /**
+     * Gets whether or not any mon is currently up for a Wonder Trade.
+     * @returns {Boolean} True if any Pokemon is up for a Wonder Trade, False otherwise.
+     */
+    isAnyMonInWonderTrade()
+    {
+        var wonderTradeData = this.state.wonderTradeData;
+        return wonderTradeData != null;
     }
 
 
@@ -1825,7 +1846,7 @@ export default class MainPage extends Component
             }).then((result) =>
             {
                 if (result.isConfirmed)
-                    this.saveAndExit();
+                    this.trySaveAndExit();
             });
 
             return;
@@ -2346,7 +2367,46 @@ export default class MainPage extends Component
     }
 
     /**
-     * Handles downloading the updated data for one of the box slots.
+     * Handles trying to download the updated data.
+     */
+    async trySaveAndExit()
+    {
+        if (this.isAnyMonInWonderTrade())
+        {
+            let pokemon = this.getMonAtBoxPos(this.state.wonderTradeData.boxType,
+                                GetOffsetFromBoxNumAndPos(this.state.wonderTradeData.boxNum, this.state.wonderTradeData.boxPos));
+
+            PopUp.fire
+            ({
+                imageUrl: GetIconSpeciesLink(pokemon),
+                imageAlt: "",
+                title: `Saving can't be done while a Pokemon is up for a Wonder Trade!\nStop trying to Wonder Trade ${GetNickname(pokemon)}?`,
+                confirmButtonText: `No, keep it going.`,
+                denyButtonText: `Cancel the trade.`,
+                showDenyButton: true,
+                scrollbarPadding: false,
+            }).then((result) =>
+            {
+                if (result.isDenied) //In this case it means agreed to cancel trade
+                {
+                    //End previous Wonder Trade
+                    if (this.state.wonderTradeData != null)
+                        this.state.wonderTradeData.socket.close();
+    
+                    this.setState({wonderTradeData: null}, async () =>
+                    {
+                        PopUp.close(); //Close the "connection lost" pop up
+                        await this.saveAndExit();
+                    });
+                }
+            });
+        }
+        else
+            await this.saveAndExit();
+    }
+
+    /**
+     * Handles downloading the updated data.
      */
     async saveAndExit()
     {
