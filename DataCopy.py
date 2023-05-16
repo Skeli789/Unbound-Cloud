d60e4f50bcdbed2f40a55e5f90fc3a729ba2639a
Skeli <sesyl.schweitzer@gmail.com>
2023-05-16 18:01:07 +0300

    Add Data Validator to DataCopy.py

--- DataCopy.py
+++ DataCopy.py
@@ -1,13 +1,15 @@
-import json, os, shutil
+import json, os, requests, shutil
+
+# Setup directories
+gCurrentDir = os.path.dirname(os.path.realpath(__file__))
+gServerDataDir = os.path.join(gCurrentDir, "server", "src", "data")
+gClientDataDir = os.path.join(gCurrentDir, "src", "data")
+gUnboundShiniesDir = os.path.join(gCurrentDir, "public", "images", "unbound_shinies")
 
 
-def main():
-    # Setup directories
-    currentDir = os.path.dirname(os.path.realpath(__file__))
-    serverDataDir = os.path.join(currentDir, "server", "src", "data")
-    clientDataDir = os.path.join(currentDir, "src", "data")
-    unboundShiniesDir = os.path.join(currentDir, "public", "images", "unbound_shinies")
-    gameDirs = [x[0] for x in os.walk(serverDataDir) if x[0] != serverDataDir]  # Ignore the parent directory
+def CopyData():
+    # Setup directories to copy from
+    gameDirs = [x[0] for x in os.walk(gServerDataDir) if x[0] != gServerDataDir]  # Ignore the parent directory
 
     # Setup files to copy over
     entireFiles = ["BaseStats.json"]  # Copy over these files directly
@@ -16,7 +18,7 @@ def main():
     # Copy all game data
     for gameDir in gameDirs:
         gameDirName = gameDir.split("\\")[-1]
-        targetDir = os.path.join(clientDataDir, gameDirName)
+        targetDir = os.path.join(gClientDataDir, gameDirName)
 
         try:
             os.makedirs(targetDir)
@@ -42,19 +44,97 @@ def main():
 
     # Generate Unbound Shinies species list
     speciesList = {}
-    for file in os.listdir(unboundShiniesDir):
+    for file in os.listdir(gUnboundShiniesDir):
         speciesList[file.split(".png")[0]] = True
-    with open(os.path.join(clientDataDir, "UnboundShinies.json"), "w") as file:
+    with open(os.path.join(gClientDataDir, "UnboundShinies.json"), "w") as file:
         file.write(json.dumps(speciesList, indent=4) + "\n")
 
     # Copy Entire Files
     entireFiles = ["ExperienceCurves.json", "SpeciesNames.json", "SpeciesToDexNum.json"]
     for file in entireFiles:
         try:
-            shutil.copy(os.path.join(serverDataDir, file), os.path.join(clientDataDir, file))
+            shutil.copy(os.path.join(gServerDataDir, file), os.path.join(gClientDataDir, file))
         except shutil.SameFileError:
             pass
     print("Data copied successfully!")
+
+
+def LoadJSONFile(filePath):
+    with open(filePath, "r") as json_file:
+        return json.load(json_file)
+
+def ValidateData():
+    errors = []
+
+    # Setup directories to validate
+    gameDirs = [x[0] for x in os.walk(gClientDataDir) if x[0] != gClientDataDir]  # Ignore the parent directory
+
+    # Load all defines
+    abilityNames = LoadJSONFile(os.path.join(gClientDataDir, "AbilityNames.json"))
+    speciesNames = LoadJSONFile(os.path.join(gClientDataDir, "SpeciesNames.json"))
+    itemNames = LoadJSONFile(os.path.join(gClientDataDir, "ItemNames.json"))
+    moveNames = LoadJSONFile(os.path.join(gClientDataDir, "MoveNames.json"))
+    typeNames = LoadJSONFile(os.path.join(gClientDataDir, "TypeNames.json"))
+    ballTypeNames = LoadJSONFile(os.path.join(gClientDataDir, "BallTypeNames.json"))
+
+    validationRules = {
+        "Species.json": (speciesNames, "Species"),
+        "Moves.json": (moveNames, "Move"),
+        "Items.json": (itemNames, "Item"),
+        "BallTypes.json": (ballTypeNames, "BallType"),
+        "BaseStats.json": (speciesNames, "Species"),
+    }
+
+    # Validate all game data
+    for gameDir in gameDirs:
+        for fileName, (defines, nameType) in validationRules.items():
+            filePath = os.path.join(gameDir, fileName)
+            allData = LoadJSONFile(filePath)
+            for dataItem in allData:
+                try:
+                    assert dataItem in defines
+                except AssertionError:
+                    errors.append(f"{nameType} \"{dataItem}\" from \"{filePath}\" not found in {nameType}Names.json")
+
+            if fileName == "BaseStats.json":
+                for species in allData:
+                    speciesData = allData[species]
+                    for abilityNum in ["ability1", "ability2", "hiddenAbility"]:
+                        try:
+                            assert speciesData[abilityNum] in abilityNames
+                        except AssertionError:
+                            errors.append(f"Ability \"{speciesData[abilityNum]}\" from \"{filePath}\" not found in AbilityNames.json")
+                    for typeNum in ["type1", "type2"]:
+                        try:
+                            assert speciesData[typeNum] in typeNames
+                        except AssertionError:
+                            errors.append(f"Type \"{speciesData[typeNum]}\" from \"{filePath}\" not found in TypeNames.json")
+
+    # Validate all item links in itemNames
+    # Too slow to be automated. Needs to be run manually
+    # for item in itemNames:
+    #     item = itemNames[item]
+    #     if "link" in item and item != "ITEM_NONE":
+    #         itemLink = item["link"]
+    #         fullLink = f"https://raw.githubusercontent.com/msikma/pokesprite/master/items/{itemLink}.png"
+    #         print("Checking link: " + fullLink)
+    #         try:
+    #             assert requests.get(fullLink).status_code == 200
+    #         except AssertionError:
+    #             errors.append(f"Item link \"{itemLink}\" from \"ItemNames.json\" not a valid link")
+
+    # Print errors if needed
+    if len(errors) != 0:
+        for error in errors:
+            print(error)
+        raise Exception("Data validation failed!")
+    else:
+        print("Data validated successfully!")
+
+
+def main():
+    CopyData()
+    ValidateData()
 
 
 if __name__ == '__main__':
